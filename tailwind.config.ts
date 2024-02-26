46ea89ad2343089b6526a27aefe6f3a9107f1ce6
Jezebel1990 <jezebelguedes@gmail.com>
2024-02-25 23:46:44 -0300

    add header components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,15 @@ const config: Config = {
     'img_bg_hero': "url('../assets/bg-hero.jpg)",
 
     extend: {
-     'primary-blue': '#106EB0',
-      'primary-gray': '#33303E',
-      'second-gray': '#4E4B59',
-      'gray-phone': '#F4F4F4',
-      'txt-gray': '#747786',
-       'opacity-gray': 'rgba(100, 80, 57, 0.1)'
-
+      colors: {
+        'primary-green': '#04e4a4',
+        'primary-turquoise': '#0cafac',
+        'primary-gray': '#33303E',
+        'second-gray': '#4E4B59',
+        'gray-phone': '#F4F4F4',
+        'txt-gray': '#747786',
+        'opacity-gray': 'rgba(100, 80, 57, 0.1)'
+      }
     },
   },
   plugins: [],
